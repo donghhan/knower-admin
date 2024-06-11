7be95c3c75067dca6e511a1cdaa5779ce6e3a275
donghhan <apensia914@gmail.com>
2024-06-11 15:22:31 +0700

    init

---
+++ config/settings/development.py
@@ -0,0 +1,14 @@
+import os
+from .base import *
+
+DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+# Databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
