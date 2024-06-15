37988197c91478f3ed3a0d80c97bf89411da704a
donghhan <apensia914@gmail.com>
2024-06-15 13:21:19 +0700

    All users list added

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
+    path(
+        "<str:user_search_keyword>/", views.UserSearchView.as_view(), name="user_search"
+    ),
     path("", views.UserListView.as_view(), name="user_list"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
+from typing import Any
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 from django.views import generic
 from . import models
 
@@ -8,3 +11,36 @@ class UserListView(generic.ListView):
     paginate_by = 20
     paginate_orphans = 5
     ordering = "created_at"
+    context_object_name = "users"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserListView, self).get_context_data(*args, **kwargs)
+        context["pathname"] = f"All {self.request.path.replace('/', '').capitalize()}"
+        return context
+
+    def get_queryset(self):
+        return models.User.objects.all()
+
+    def paginate_queryset(self, queryset, page_size):
+        return super().paginate_queryset(queryset, page_size)
+
+
+class UserSearchView(generic.ListView):
+    model = models.User
+
+    def get_queryset(self):
+        user_search_keyword = self.kwargs.get("user_search_keyword")
+        if user_search_keyword:
+            queryset = models.User.objects.get(
+                Q(email__icontains=user_search_keyword)
+                | Q(first_name__icontains=user_search_keyword)
+                | Q(last_name__icontains=user_search_keyword)
+            )
+        else:
+            queryset = models.User.objects.none()
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserSearchView, self).get_context_data(*args, **kwargs)
+        context["searched_user"] = self.get_queryset()
+        return context
